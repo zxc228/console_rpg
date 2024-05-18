e8f1e474cc291aef8de5fe58bf7c1342053e0260
zxc228 <70976667+zxc228@users.noreply.github.com>
2024-05-18 19:04:08 +0200

    Update DefensiveStrategy.java

--- src/strategies/DefensiveStrategy.java
+++ src/strategies/DefensiveStrategy.java
@@ -6,6 +6,6 @@ public class DefensiveStrategy implements Strategy {
     @Override
     public void execute(Character character) {
         System.out.println(character.getName() + " is using defensive strategy.");
-        character.setEndurance(character.getEndurance() + 2);
+        character.setEndurance(character.getEndurance() + 3);
     }
 }
